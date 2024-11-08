7f3ce091cc23286199b437125691e129964f0078
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2024-11-08 13:35:01 +0000

    Action

--- Redux_Tut/src/Components/Home.jsx
+++ Redux_Tut/src/Components/Home.jsx
@@ -9,7 +9,7 @@ function Home(){
             <div>
             <img className="logo" src="https://cdn-icons-png.freepik.com/512/7835/7835563.png" alt="" />
             </div>
-            <h1>Welcome to my home page</h1>
+            <h1>E-Sale Components</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
                     <img src="https://www.apple.com/v/iphone-16-pro/c/images/overview/apple-intelligence/apple_intelligence_endframe__ksa4clua0duu_xlarge.jpg" alt="" />
